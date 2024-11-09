1f7a6dc93d5fdd915fea487b7afe10ccb3d5c9da
inori.dokusha <inori.dokusha@gmail.com>
2024-11-09 23:31:35 +0000

    delete and add

---
+++ .idx/dev.nix
@@ -0,0 +1,58 @@
+# To learn more about how to use Nix to configure your environment
+# see: https://developers.google.com/idx/guides/customize-idx-env
+{ pkgs, ... }: {
+  # Which nixpkgs channel to use.
+  channel = "stable-24.05"; # or "unstable"
+
+  # Use https://search.nixos.org/packages to find packages
+  packages = [
+    pkgs.nodejs_22
+  ];
+
+  # Sets environment variables in the workspace
+  env = {};
+  idx = {
+    # Search for the extensions you want on https://open-vsx.org/ and use "publisher.id"
+    extensions = [  
+    "PKief.material-icon-theme"
+    "adpyke.codesnap"
+    "christian-kohler.path-intellisense"
+    "CoenraadS.bracket-pair-colorizer"
+    "CoenraadS.bracket-pair-colorizer-2"
+    "dbaeumer.vscode-eslint"
+    "esbenp.prettier-vscode"
+    "formulahendry.auto-close-tag"
+    "formulahendry.auto-complete-tag"
+    "formulahendry.auto-rename-tag"
+    "ritwickdey.LiveServer"
+    "shd101wyy.markdown-preview-enhanced"
+    "usernamehw.errorlens"
+    ];
+
+    # Enable previews
+    previews = {
+      enable = true;
+      previews = {
+        # web = {
+        #   # Example: run "npm run dev" with PORT set to IDX's defined port for previews,
+        #   # and show it in IDX's web preview panel
+        #   command = ["npm" "run" "dev"];
+        #   manager = "web";
+        #   env = {
+        #     # Environment variables to set for your server
+        #     PORT = "$PORT";
+        #   };
+        # };
+      };
+    };
+
+    # Workspace lifecycle hooks
+    workspace = {
+      # Runs when a workspace is first created
+      onCreate = {
+        # Example: install JS dependencies from NPM
+        install-eslint = "npm install -g eslint";
+      };
+    };
+  };
+}
