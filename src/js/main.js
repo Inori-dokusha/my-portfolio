eb1ff78247270acae10afd85f90f30bdb1b15e93
inori.dokusha <inori.dokusha@gmail.com>
2024-11-17 07:49:11 +0000

    clear script

--- src/js/main.js
+++
@@ -1,7 +0,0 @@
-// let buttonNav = document.querySelector('.button-nav').addEventListener('click', () => {
-//   document.querySelector('.line').classList.toggle('rotate');
-// });
-let buttonNav = document.querySelector('.button-nav');
-buttonNav.addEventListener('click', () => {
-  document.querySelectorAll('.line').classList.toggle('rotate');
-});
